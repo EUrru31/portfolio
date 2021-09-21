62b26e2a6449ce4d9d40a040f93bb6e7909914c1
Urru <etienne.urru@gmail.com>
2021-09-21 15:43:31 +0200

    contact mediaqueries

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -9,54 +9,45 @@
         </div>
 
         <div class="contact">
-            <div class="contactPage">
-                <div class="container">
-                    <div class="bloc">
-                        <div>
-                            <h3>Demander un devis</h3>
-                            <p>
-                                Si vous souhaitez me contacter afin de réaliser
-                                vos projets:
-                            </p>
+            <div>
+                <h3>Demander un devis</h3>
+                <p>
+                    Si vous souhaitez me contacter afin de réaliser vos projets:
+                </p>
 
-                            <div class="contactInformation">
-                                <i class="fas fa-mobile-alt"></i>
-                                <div class="infos">
-                                    <p class="infosTitle">Téléphone</p>
-                                    <p>07 83 01 88 11</p>
-                                </div>
-                            </div>
-                            <div class="contactInformation">
-                                <i class="fas fa-at"></i>
-                                <div class="infos">
-                                    <p class="infosTitle">Email:</p>
-                                    <p>etienne.urru@cdev.fr</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="reseau">
-                                <h4>Réseaux</h4>
-                                <p>Retrouver moi également sur les réseaux:</p>
-                                <div class="reseauList">
-                                    <a
-                                        href="mailto=etienne.urru@conceptdev-web.fr"
-                                        target="_blank"
-                                        ><i class="fas fa-envelope-square"></i
-                                    ></a>
-                                    <a
-                                        href="https://www.linkedin.com/in/etienneurru"
-                                        target="_blank"
-                                        ><i class="fab fa-linkedin"></i
-                                    ></a>
-                                    <a
-                                        href="https://github.com/EUrru31"
-                                        target="_blank"
-                                        ><i class="fab fa-github-square"></i
-                                    ></a>
-                                </div>
-                            </div>
-                        </div>
+                <div class="contactInformation">
+                    <i class="fas fa-mobile-alt"></i>
+                    <div class="infos">
+                        <p class="infosTitle">Téléphone</p>
+                        <p>07 83 01 88 11</p>
+                    </div>
+                </div>
+                <div class="contactInformation">
+                    <i class="fas fa-at"></i>
+                    <div class="infos">
+                        <p class="infosTitle">Email:</p>
+                        <p>etienne.urru@cdev.fr</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="reseau">
+                    <h4>Réseaux</h4>
+                    <p>Retrouver moi également sur les réseaux:</p>
+                    <div class="reseauList">
+                        <a
+                            href="mailto=etienne.urru@conceptdev-web.fr"
+                            target="_blank"
+                            ><i class="fas fa-envelope-square"></i
+                        ></a>
+                        <a
+                            href="https://www.linkedin.com/in/etienneurru"
+                            target="_blank"
+                            ><i class="fab fa-linkedin"></i
+                        ></a>
+                        <a href="https://github.com/EUrru31" target="_blank"
+                            ><i class="fab fa-github-square"></i
+                        ></a>
                     </div>
                 </div>
             </div>
@@ -89,5 +80,15 @@ a {
     margin-left: 20px;
     margin-right: 20px;
     color: white;
+}
+
+@media screen and (min-width: 768px) {
+    .contact {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        margin-top: 70px;
+        margin-bottom: 70px;
+    }
 }
 </style>
