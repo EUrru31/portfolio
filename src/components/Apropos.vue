bcda37d306a12e9ca6fea3983941d890a8e11af2
Urru <etienne.urru@gmail.com>
2021-09-21 15:28:16 +0200

    Présentation mediaqueries

--- src/components/Apropos.vue
+++ src/components/Apropos.vue
@@ -17,21 +17,22 @@
                     Bonjour, je suis Etienne Urru
                 </p>
                 <p>
-                    Ayant travaillé 14 ans dans le domaine de la logistique, j'ai pu acquérir
-                    différentes compétences comme le respect de la hierarchie,
-                    le sérieux dans le travail ainsi que l'organisation.
+                    Ayant travaillé 14 ans dans le domaine de la logistique,
+                    j'ai pu acquérir différentes compétences comme le respect de
+                    la hierarchie, le sérieux dans le travail ainsi que
+                    l'organisation.
                 </p>
                 <p>
                     Il y a un an, j'ai pris la décision de commencer ma
-                    reconversion professionnelle. Je me suis donc lancé dans un projet qui me
-                    tenait à coeur depuis plusieurs années, le développement
-                    web. Suite a cette décision, je me suis formé en tant que
-                    développeur web fullstack.
+                    reconversion professionnelle. Je me suis donc lancé dans un
+                    projet qui me tenait à coeur depuis plusieurs années, le
+                    développement web. Suite a cette décision, je me suis formé
+                    en tant que développeur web fullstack.
                 </p>
                 <p>
-                    Ayant désormais obtenu mon diplôme, je m'épanouis
-                    dans cette nouvelle vocation et je continue dans cette voie
-                    avec envie et détermination.
+                    Ayant désormais obtenu mon diplôme, je m'épanouis dans cette
+                    nouvelle vocation et je continue dans cette voie avec envie
+                    et détermination.
                 </p>
             </div>
         </div>
@@ -58,5 +59,22 @@ h2 {
 }
 .presentationText {
     margin-top: 40px;
+}
+@media screen and (min-width: 1025px) {
+    .presentationPhoto {
+        display: block;
+        margin-left: 150px;
+    }
+    .presentationPage {
+        display: flex;
+        flex-direction: row;
+        margin-bottom: 100px;
+        margin-top: 100px;
+    }
+    .presentationText {
+        width: 50%;
+        margin-left: auto;
+        margin-right: 150px;
+    }
 }
 </style>
